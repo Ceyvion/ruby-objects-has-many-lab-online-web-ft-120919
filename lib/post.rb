4cc26c208f6ae60f2252da048e3c4a6a3e476ca7
Ceyvion Biggs <ceyvionb@gmail.com>
2020-01-07 20:59:49 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,27 @@
 class Post 
   
+  attr_accessor :t, :artist 
+
+@@all = []
+
+def initialize(name)
+  @name = name 
+  @@all << self 
+end 
+
+def self.all  
+  @@all
+end 
+
+def artist_name
+  if self.artist == nil 
+    self.artist 
+  else 
+    self.artist.name 
+  end 
+end 
+
+  
   
 
   
