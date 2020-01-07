d0c4db683917991b873085ee4eed66a75365b818
Ceyvion Biggs <ceyvionb@gmail.com>
2020-01-07 20:49:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,26 @@
 class Song 
-  
+
+attr_accessor :name, :artist 
+
 @@all = []
 
-def initialize(song)
-  
-  @@song = song 
+def initialize(name)
+  @name = name 
+  @@all << self 
 end 
+
+def self.all  
+  @@all
+end 
+
+def artist_name
+  if self.artist == nil 
+    self.artist 
+  else 
+    self.artist.name 
+  end 
+end 
+
 
   
   
