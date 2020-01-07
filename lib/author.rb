103f2878c73cbd0e7ba15e4c8dc4dfe577e4e75d
Ceyvion Biggs <ceyvionb@gmail.com>
2020-01-07 21:07:16 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,30 +1,29 @@
 class Author 
   
-  
-  @@all = []
-  
   attr_accessor :name 
+  
+@@all = []
   
 def initialize(name)
   @name = name 
 end 
 
- def songs
+def songs
     Song.all.select {|song| song.artist==self}
-  end  
+end  
   
-  def add_song(song)
+def add_song(song)
     song.artist = self
-  end  
+end  
   
-  def add_song_by_name(name)
+def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
-  end  
+end  
   
-  def self.song_count
+def self.song_count
     Song.all.length 
-  end
+end
 
 
 
